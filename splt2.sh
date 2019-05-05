9851da69d9e0679866b05f3902ab34f36a6d86e7
yiyusheng@vps <yiyusheng.hust@gmail.com>
2019-05-05 08:20:21 +0000

    add script

---
+++ splt2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo `date`
+
+/home/yiyusheng/anaconda2/bin/python /home/yiyusheng/Code/Python/splatnet2statink/splatnet2statink.py -r
+
+/home/yiyusheng/anaconda2/bin/python /home/yiyusheng/Code/Python/splatnet2statink/splatnet2statink.py --salmon
